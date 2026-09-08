b99624e248eb5a6b2e59f7dd9e17bf11ceb7c253
Denise Ugalde <vanesa_7_denise@hotmail.com>
2026-09-08 15:28:14 -0600

    creación de modelos

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,75 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_08_180305) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_08_193928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "case_files", force: :cascade do |t|
+    t.bigint "case_id", null: false
+    t.datetime "created_at", null: false
+    t.string "document_number"
+    t.string "file_type"
+    t.string "state"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.index ["case_id"], name: "index_case_files_on_case_id"
+  end
+
+  create_table "cases", force: :cascade do |t|
+    t.boolean "consent"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.bigint "representative_id"
+    t.text "summary"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["representative_id"], name: "index_cases_on_representative_id"
+    t.index ["user_id"], name: "index_cases_on_user_id"
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_chats_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "chat_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.string "role"
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+  end
+
   create_table "users", force: :cascade do |t|
+    t.integer "age"
+    t.string "city"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "gender"
+    t.string "last_name"
+    t.string "name"
+    t.string "phone_number"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.string "role"
+    t.string "sex"
+    t.string "state"
     t.datetime "updated_at", null: false
+    t.string "user_contry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "case_files", "cases"
+  add_foreign_key "cases", "users"
+  add_foreign_key "cases", "users", column: "representative_id"
+  add_foreign_key "chats", "users"
+  add_foreign_key "messages", "chats"
 end
