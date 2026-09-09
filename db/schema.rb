6bfa658186a556f383dfd6160f7473f2d63c752b
Jillian Menichetti <jmenichetti16@gmail.com>
2026-09-09 15:56:07 -0300

    Agrega nivel de riesgo y pundit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_09_174405) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_09_184240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_09_174405) do
     t.text "content"
     t.datetime "created_at", null: false
     t.bigint "representative_id"
+    t.string "risk_level"
     t.string "status", default: "pending", null: false
     t.text "summary"
     t.datetime "updated_at", null: false
