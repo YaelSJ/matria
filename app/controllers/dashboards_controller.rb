b12b9afa177dc6774ff3b56cfe929cbd30e21c3b
Jillian Menichetti <jmenichetti16@gmail.com>
2026-09-09 18:35:12 -0300

    Crear vista de representante, añadir paleta de colores a archivo _color.scss

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,12 +5,19 @@ class DashboardsController < ApplicationController
       authorize @case, :show? if @case
     elsif current_user.representative?
       @state_cases = policy_scope(Case)
-                    .includes(:representative)
-                    .in_order_of(:risk_level, %w[critical high medium low])
+                     .includes(:representative)
+                     .in_order_of(:risk_level, %w[critical high medium low])
 
       @assigned_cases = @state_cases.where(
         representative_id: current_user.id
       )
+      @active_tab = params[:tab] == "state" ? "state" : "assigned"
+
+      @cases = if @active_tab == "state"
+                 @state_cases
+               else
+                 @assigned_cases
+               end
     else
       head :forbidden
     end
