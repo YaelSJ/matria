69972c67be17bebdc1cc51fa64ae42c8346ec23a
Jillian Menichetti <jmenichetti16@gmail.com>
2026-09-11 16:45:11 -0300

    mejora del flujo y implementar + frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users,
              controllers: { registrations: "users/registrations" }
-  root to: "pages#home"
 
+  authenticated :user do
+    root to: "dashboards#show", as: :authenticated_root
+  end
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 
